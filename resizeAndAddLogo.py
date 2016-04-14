7480d83d746d485c01c2f94832e4afb185ef5ef6
Derrick Cates <dcates@mail.valenciacollege.edu>
2016-04-13 22:25:55 -0400

    try 4

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -14,7 +14,7 @@ for filename in os.listdir('./originals/'): #loops through originals folder to v
     if not (filename.endswith('.png') or filename.endswith('.jpg')) \
         or filename == LOGO_FILENAME:
         continue
-    im = Image.open(os.path.join('./originals/' + (filename)))#opens the images inside of the originals folder 
+    im = Image.open(os.path.join('./originals/' + filename))#opens the images inside of the originals folder 
     width, height = im.size  #assigns width and height to im.size
 
 
@@ -27,13 +27,13 @@ for filename in os.listdir('./originals/'): #loops through originals folder to v
             height = SQUARE_FIT_SIZE
         
 
-        print('Resizing %s...' % (filename))  #when program is running it prints that it is resizing the picture
-        im = im.resize((width, height))
+    print('Resizing %s...' % (filename))  #when program is running it prints that it is resizing the picture
+    im = im.resize((width, height))
 
-        print('Adding logo to %s...' % (filename))  #prints that it is adding the logo to the picture 
-        im.paste(logoIm, (width - logoWidth, height - logoHeight) , logoIm)
+    print('Adding logo to %s...' % (filename))  #prints that it is adding the logo to the picture 
+    im.paste(logoIm, (width - logoWidth, height - logoHeight) , logoIm)
 
-        im.save(os.path.join('withLogo', (filename)))  #saves the new picture with logo to the newly created directory/ folder withLogo
+    im.save(os.path.join('withLogo', filename))  #saves the new picture with logo to the newly created directory/ folder withLogo
 
 
 
