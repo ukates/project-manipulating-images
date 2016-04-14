d0ff5c9d88987564257912df1c0dd83aded261d6
Derrick Cates <dcates@mail.valenciacollege.edu>
2016-04-13 23:45:24 -0400

    zzzz

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -11,29 +11,29 @@ os.makedirs(os.path.join('withLogo'), exist_ok=True) #creates folder titled with
 
 
 for filename in os.listdir('./originals/'): #loops through originals folder to view the contents inside
-        if not (filename.endswith('.png') or filename.endswith('.jpg')) or filename == LOGO_FILENAME:
-            continue
+    if not (filename.endswith('.png') or filename.endswith('.jpg')) or filename == LOGO_FILENAME:
+         continue
 
-        im = Image.open(os.path.join('./originals/', filename))#opens the images inside of the originals folder 
-        width, height = im.size  #assigns width and height to im.size
+    im = Image.open(os.path.join('./originals/', filename))#opens the images inside of the originals folder 
+    width, height = im.size  #assigns width and height to im.size
 
 
-        if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:  #if picture is too big this will resize the picture to fit within the guidelines set to square_fit_size, which in this case is 300
-            if width > height:
-                height = int((SQUARE_FIT_SIZE / width) * height) 
-                width = int(SQUARE_FIT_SIZE)
+    if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:  #if picture is too big this will resize the picture to fit within the guidelines set to square_fit_size, which in this case is 300
+        if width > height:
+            height = int((SQUARE_FIT_SIZE / width) * height) 
+            width = int(SQUARE_FIT_SIZE)
         else:
             width = int((SQUARE_FIT_SIZE / height) * width)
             height = int(SQUARE_FIT_SIZE)
         
 
-        print('Resizing %s...' % (filename))  #when program is running it prints that it is resizing the picture
-        im = im.resize((width, height))
+    print('Resizing %s...' % (filename))  #when program is running it prints that it is resizing the picture
+    im = im.resize((width, height))
 
-        print('Adding logo to %s...' % (filename))  #prints that it is adding the logo to the picture 
-        im.paste(logoIm, (width - logoWidth, height - logoHeight) , logoIm)
+    print('Adding logo to %s...' % (filename))  #prints that it is adding the logo to the picture 
+    im.paste(logoIm, (width - logoWidth, height - logoHeight) , logoIm)
 
-        im.save(os.path.join('withLogo', filename))  #saves the new picture with logo to the newly created directory/ folder withLogo
+im.save(os.path.join('withLogo', filename))  #saves the new picture with logo to the newly created directory/ folder withLogo
 
 
 
