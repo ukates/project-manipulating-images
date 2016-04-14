d71e17eac1bcf558b120167562d351b4784e16d5
Derrick Cates <dcates@mail.valenciacollege.edu>
2016-04-13 23:31:34 -0400

    i lied one more try, hopefully i figured out what was wrong with my spacing

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -22,16 +22,16 @@ for filename in os.listdir('./originals/'): #loops through originals folder to v
         if width > height:
             height = int((SQUARE_FIT_SIZE / width) * height) 
             width = int(SQUARE_FIT_SIZE)
-        else:
-            width = int((SQUARE_FIT_SIZE / height) * width)
-            height = int(SQUARE_FIT_SIZE)
+    else:
+        width = int((SQUARE_FIT_SIZE / height) * width)
+        height = int(SQUARE_FIT_SIZE)
         
 
-        print('Resizing %s...' % (filename))  #when program is running it prints that it is resizing the picture
-        im = im.resize((width, height))
+    print('Resizing %s...' % (filename))  #when program is running it prints that it is resizing the picture
+    im = im.resize((width, height))
 
-        print('Adding logo to %s...' % (filename))  #prints that it is adding the logo to the picture 
-        im.paste(logoIm, (width - logoWidth, height - logoHeight) , logoIm)
+    print('Adding logo to %s...' % (filename))  #prints that it is adding the logo to the picture 
+    im.paste(logoIm, (width - logoWidth, height - logoHeight) , logoIm)
 
 im.save(os.path.join('withLogo', filename))  #saves the new picture with logo to the newly created directory/ folder withLogo
 
